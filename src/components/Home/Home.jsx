3fa17763446e7c03f7baa205ea44aac3dbedff5e
hirendranath <contact.hirendranath@gmail.com>
2023-09-16 14:12:52 +0600

    fake data loaded dynamically

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+/* eslint-disable react/jsx-key */
+import React, { useEffect, useState } from 'react';
 import "./Home.css"
 const Home = () => {
+
+    const [allCourse, setAllCourse] = useState([]);
+
+    useEffect(() => {
+        fetch("./data.json")
+            .then((res) => res.json())
+            .then((data) => setAllCourse(data));
+    }, []);
+
+console.log(allCourse);
+
     return (
         <div className="container">
-            <div className="card-container">
-                <div className="card">
-                    <div className="card-image">
-                        <img className='photo' src="https://i.ibb.co/68dK4h5/47094270.jpg" alt="" />
-                    </div>
-                    <h2>Introduction to C Programming</h2>
-                    <p><small>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, quo.</small></p>
-                    <div className="info">
-                        <p> Price : 19000</p>
-                        <p> Credit : 5hr </p>
-                    </div>
-                    <button className='course-select-btn' >Select</button>
+            <div className="card-main-container">
+                <div className="card-container">
+                    {
+                        allCourse.map(course => (
+                            <div key={course.id} className="card">
+                                <div className="card-image">
+                                    <img className='photo' 
+                                    src={course.image}
+                                    alt="" />
+                                </div>
+                                <h3>{course.name}</h3>
+                                <p><small>{course.details}</small></p>
+                                <div className="info">
+                                    <p> Price : {course.Price} </p>
+                                    <p> Credit : {course.Credit} </p>
+                                </div>
+                                <button className='course-select-btn' >Select</button>
+                            </div>
+                        ))
+
+                    }
+                </div>
+                <div className="card2">
+                    <h2>Hi</h2>
                 </div>
 
             </div>
